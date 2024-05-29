529a4254197e3aa3a04bd08aa113ba05ee4f302a
kannurir <31421469+kannurir@users.noreply.github.com>
2024-05-29 13:50:36 -0400

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,13 @@
+// server/models/user.js
+     const mongoose = require('mongoose');
 
+     const userSchema = new mongoose.Schema({
+         firstName: String,
+         lastName: String,
+         email: { type: String, unique: true },
+         password: String
+     });
+
+     const User = mongoose.model('User', userSchema);
+
+     module.exports = User;
